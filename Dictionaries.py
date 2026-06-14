7640dea20ce5b0fd64bb169fb40c9c6454d9e16b
Arnab Choudhury <arnab2011choudhury@gmail.com>
2026-06-14 18:32:32 +0530

    Added player class, enemy class and a dictionary

---
+++ Dictionaries.py
@@ -0,0 +1,11 @@
+armors = {
+    "" : ""
+}
+
+characters = {
+    "" : ""
+}
+
+weapons = {
+    "" : ""
+}
---
+++ enemy.py
@@ -0,0 +1,15 @@
+import Dictionaries as dict
+
+class Enemy:
+    def __init__(self, health, weapon, armor):
+        self.harm = None
+        self.damage = None
+        self.health = health
+        self.weapon = weapon
+        self.armor = armor
+
+    def damage_dealt(self):
+        self.damage = dict.weapons.get(self.weapon)
+
+    def harm_received(self, enemyDamage):
+        self.harm = enemyDamage - dict.armors.get(self.armor)

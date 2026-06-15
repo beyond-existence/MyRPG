88134f56419e3b95370d865fa30706c1d886491e
Arnab Choudhury <arnab2011choudhury@gmail.com>
2026-06-15 13:16:30 +0530

    "More stuff in Dictionaries.py"

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,11 +1,15 @@
 armors = {
 "Knight's armor" : 100,
-"Mage's cap" : 80,
+"Mage's cloak" : 80,
 "Giant's clothes" : 50,
+"Archer's cap" : 40,
+"Drown's skin" : 70,
 }
 
 weapons = {
 "Knight's sword" : 80,
 "Mage's wand" : 100,
-"Giant's club" : 50,
+"Giant's Battleaxe" : 50,
+"Archer's bow" : 50,
+"Drown's trident" : 90,
 }
--- enemy.py
+++ enemy.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 
 class Enemy(ABC):
-    def __init__(self, health, weapon, armor):
+    def __init__(self, health, weapon, armor, ):
         self.harm = None
         self.damage = None
         self.health = health
@@ -36,6 +36,14 @@ class Knight(Enemy):
 
 
 class Giant(Enemy):
+    def damage_dealt(self):
+        self.damage = dict.weapons.get(self.weapon)
+
+    def harm_received(self, enemyDamage):
+        self.harm = enemyDamage - dict.armors.get(self.armor)
+
+
+class Archer(Enemy):
     def damage_dealt(self):
         self.damage = dict.weapons.get(self.weapon)
 

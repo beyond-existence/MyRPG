b333841dc5ee51d4ad87fd1e07cf184c666600f2
Arnab Choudhury <arnab2011choudhury@gmail.com>
2026-06-15 13:01:10 +0530

    Added 3 classes in enemy.py and some basic elements in dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,11 +1,11 @@
 armors = {
-    "" : ""
-}
-
-characters = {
-    "" : ""
+"Knight's armor" : 100,
+"Mage's cap" : 80,
+"Giant's clothes" : 50,
 }
 
 weapons = {
-    "" : ""
+"Knight's sword" : 80,
+"Mage's wand" : 100,
+"Giant's club" : 50,
 }
--- enemy.py
+++ enemy.py
@@ -1,6 +1,8 @@
 import Dictionaries as dict
+from abc import ABC, abstractmethod
 
-class Enemy:
+
+class Enemy(ABC):
     def __init__(self, health, weapon, armor):
         self.harm = None
         self.damage = None
@@ -8,6 +10,32 @@ class Enemy:
         self.weapon = weapon
         self.armor = armor
 
+    @abstractmethod
+    def damage_dealt(self):
+        pass
+
+    @abstractmethod
+    def harm_received(self, enemyDamage):
+        pass
+
+
+class Mage(Enemy):
+    def damage_dealt(self):
+        self.damage = dict.weapons.get(self.weapon)
+
+    def harm_received(self, enemyDamage):
+        self.harm = enemyDamage - dict.armors.get(self.armor)
+
+
+class Knight(Enemy):
+    def damage_dealt(self):
+        self.damage = dict.weapons.get(self.weapon)
+
+    def harm_received(self, enemyDamage):
+        self.harm = enemyDamage - dict.armors.get(self.armor)
+
+
+class Giant(Enemy):
     def damage_dealt(self):
         self.damage = dict.weapons.get(self.weapon)
 
